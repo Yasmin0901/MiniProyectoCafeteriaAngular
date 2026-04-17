8e40e594909a51b055dfad79286039394050b4bc
Yasmín <yasdelgado09@gmail.com>
2026-04-17 13:36:05 -0600

    arreglo conexion mysql con ssl

--- config/db.js
+++ config/db.js
@@ -4,16 +4,10 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error MySQL:", err.message);
-    return;
+  ssl: {
+    rejectUnauthorized: false
   }
-
-  console.log("MySQL conectado");
 });
-
-module.exports = connection;
