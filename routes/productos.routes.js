0281c8915eb473181a0ef0456888f3c5f19e7bdf
Yasmín <yasdelgado09@gmail.com>
2026-04-18 10:05:23 -0600

    Rectificando mas cosillas

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -8,10 +8,10 @@ router.get('/', controller.getAll);
 
 router.get('/categoria/:categoria', controller.getByCategoria);
 
-router.get('/:id', controller.getById);
+router.get('/:idPlatillo', controller.getById);
 
 router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.put('/:idPlatillo', controller.update);
+router.delete('/:idPlatillo', controller.delete);
 
 module.exports = router;
